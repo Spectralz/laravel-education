7f12f7aff80199cae72ae43787ce5745510af73a
Spectralz <you@example.com>
2022-07-17 14:41:14 +0300

    Update category

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,23 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
 Route::group(['namespace' => '\App\Http\Controllers\Blog', 'prefix' => 'blog'], function () {
     Route::resource('posts' , 'PostController')->names('blog.posts');
 });
 
+// Админка Блога
+$groupData = [
+    'namespace' => '\App\Http\Controllers\Blog\Admin',
+    'prefix'    => 'admin/blog',
+];
+Route::group($groupData, function(){
+//Blog Category
+$methods = ['index', 'edit', 'update', 'create', 'store',];
+Route::resource('categories' , 'CategoryController')
+    ->only($methods)
+    ->names('blog.admin.categories');
+});
